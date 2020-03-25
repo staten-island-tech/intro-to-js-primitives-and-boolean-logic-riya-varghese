de950827edd361ef2a677a96ab80c741a4309217
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-03-25 10:30:14 -0400

    Update app.js

---
+++ .vscode/app.js
@@ -0,0 +1,66 @@
+/*let string = "I am a string";
+let sentence ="Hello Angel " + "and Vicky";
+Let combine = string + sentence; 
+let student = "Sandra";
+let student2 = "Gia";
+let courseName = "AP CSP"
+let teacherName = "Whalen"
+let sentence =
+    student +
+    " and " +
+    student2 +
+    " are taking " +
+    courseName +
+    " with " +
+    teacherName;
+
+
+Let betterSentence = '${student} and ${student2} are taking ${courseName} with ${teacherName} next year'; 
+*/
+/////////////////Number and Intergers
+/* Let year = 2020;
+Let nextYear = year - 1;
+Let past = 2020 / 2;
+Let future = year * 2; */
+
+///BooLeans
+//Let trueStatement = false;
+
+////////Null and undefined
+//Let richard;
+
+///////BooLean Logic or if/else
+
+//let age = 18;
+//let baoSquared = "Bao Bao";
+//console.Log(baoSquared === "bao Bao";
+//console.Log(age == "17");
+
+//let age = 45;
+//if (age 18) {
+//    console.log("You are a child");
+//} else if (age == 18) {
+//    console.log("Happy Birthday");
+//} else {
+//    console.log("You are over 18");
+//}
+//red, green, yellow
+/*let light = "Edis";
+if (light === "green") {
+    console.log("go")
+}  else if (light === "green") {
+    console.log("Go");
+} else {
+    console.log("Stop");
+   
+   Let kristyIntolerant = true;
+   Let baoBaoIntolerant = false;
+
+   if (kristyIntolerant === true && baoBaoIntolerant === true) {
+    console.log("Get almond milk");
+   } else if (kristyIntolerant === true || baoBaoIntolerant === true) {
+        console.log("Get both milk");
+   } else {
+    console.log("Just regular milk");
+   }
+    */
