e6db38c388812ddeb0a2faa527d6e7e3ad6eca4e
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-04-03 00:36:08 -0400

    Update app.js

--- .vscode/app.js
+++ .vscode/app.js
@@ -64,3 +64,88 @@ if (light === "green") {
     console.log("Just regular milk");
    }
     */
+   /* Let temp = 72;
+   if (temp < 68){
+       console.log("Too cold");
+   } else if (temp >= 68 && temp <= 72) {
+    console.log("perfect");
+   } else if (temp > 72 && temp <= 76) {
+        console.log("is warm");
+   } else {
+    console.log("Iss hot");
+   }
+*/
+/*function test() {
+    console.log("I'm a function");
+}
+test();
+*/
+/* const add = function(x, y) {
+    return x + y;//return ends the function
+}
+console.log(add(100, 20))(
+const name = "Richard"; //cannont re-define const
+name = "Richard";
+*/
+/*const hello = function() {
+    console.log("hello");
+};
+const bye = function() {
+    console.log("bye");
+};
+const hiBye = function() {
+    hello();
+    bye();
+    hello();
+    bye();
+};
+hiBye(); 
+*/
+/* const name = "Tharushi"; //global variable or global space
+
+function displacement() {
+    const name ="kelly"; //local or function scope
+    alert(name);
+    function sun() {
+        console.log(name);//inner function has access to outer function
+    }
+    return sun;
+} 
+const myFunc = displayName();
+myFunc();*/
+//closure
+
+//arrays
+
+/* const names = ["Aarthi", "Vicky", "Vincent", "Doyle", "Kelly"];
+//names.push("karen");
+names.forEach(el => console.log(el)); */
+//spread operator
+//https://developer.mozilla.org and go to arrays
+// "=>" functions need no return
+
+/* const numbers = [1,2,3];
+const add = function(x, y , z) {
+    return x + y + z;
+};
+console.log(add(...nubmers)); */
+
+// let students = ["Riya", "Omar", "Alan", "Jiayu", "Sun", "Bao", "Jacob", "Whalen"]
+/* const newArr = students.filter(word => word.length > 5); // for only 4 letter put "==="
+console.log(newArr); */
+    //.push(); //adding another varriable
+    //.forEach();
+    //.map();
+
+// check "includes" junshen
+/*if (students.includes("Junshen")){
+    console.log(true);
+}   else {
+    console.log(false);
+}
+//check if it incudes Alan or Savva  "some"
+    if (students.includes("Alan", "Savva")){
+        console.log(false);
+    }   else {
+        console.log(true);
+    }  */
